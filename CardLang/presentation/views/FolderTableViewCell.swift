7c4ae276f21fe469b72b13c82b78e18123064a32
Leonid06 <x.worklene@gmail.com>
2022-12-04 23:53:28 +0300

    added blank for table views

---
+++ CardLang/presentation/views/FolderTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FolderTableViewCell.swift
+//  CardLang
+//
+//  Created by Leonid on 04.12.2022.
+//
+
+import UIKit
+
+class FolderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
