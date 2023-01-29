6e1e7bc5f3999b038646d895ed4ef9053122f02d
Leonid06 <x.worklene@gmail.com>
2023-01-29 20:58:43 +0300

    added required files

---
+++ CardLang/data/repositories/AuthenticationRepository.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationRepository.swift
+//  CardLang
+//
+//  Created by Leonid on 29.01.2023.
+//
+
+import Foundation
---
+++ CardLang/presentation/controllers /Authentication/AuthenticationNavigationController.swift
@@ -0,0 +1,29 @@
+//
+//  AuthenticationNavigationController.swift
+//  CardLang
+//
+//  Created by Leonid on 29.01.2023.
+//
+
+import UIKit
+
+class AuthenticationNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
