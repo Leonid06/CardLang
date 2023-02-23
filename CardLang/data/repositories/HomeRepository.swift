e967245a33b1410e9edc18e8d26d589ea6ee063b
Leonid06 <x.worklene@gmail.com>
2023-02-23 22:47:53 +0300

    added recent objects repository methods

--- CardLang/data/repositories/HomeRepository.swift
+++ CardLang/data/repositories/HomeRepository.swift
@@ -6,11 +6,33 @@
 //
 
 import Foundation
+import RealmSwift
 
 
 class HomeRepository {
     static let shared = HomeRepository()
+    private let realmService = RealmService.shared
     
     
+    func getRecentlyVisitedWordSets() -> Results<WordSet>? {
+        if let realm = realmService.getRealm() {
+            var recentWordSets = realm.objects(WordSet.self).sorted(byKeyPath: "dateTimeLastVisited")
+            if recentWordSets.isEmpty {
+                recentWordSets = realm.objects(WordSet.self)
+            }
+            return recentWordSets
+        }
+        return nil
+    }
     
+    func getRecentlyVisitedFolders() -> Results<Folder>? {
+        if let realm = realmService.getRealm() {
+            var recentFolders = realm.objects(Folder.self).sorted(byKeyPath: "dateTimeLastVisited")
+            if recentFolders.isEmpty {
+                recentFolders = realm.objects(Folder.self)
+            }
+            return recentFolders
+        }
+        return nil
+    }
 }
